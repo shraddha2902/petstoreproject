055bab0059d5d93bee59c908d3dc1f96ffe87916
shraddha2902 <shraddhanangare963@gmail.com>
2023-11-27 18:06:27 +0530

    first commit

---
+++ src/main/java/com/itvedant/petstoreproject/PetstoreprojectApplication.java
@@ -0,0 +1,13 @@
+package com.itvedant.petstoreproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PetstoreprojectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PetstoreprojectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/itvedant/petstoreproject/PetstoreprojectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.itvedant.petstoreproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PetstoreprojectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
